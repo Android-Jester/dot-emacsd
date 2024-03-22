3c7018ec2dc8501bfa2ead654a5bd97108330f0e
Duah Kwadwo Adjei <duah14@outlook.com>
2024-03-22 06:49:35 +0000

    initial commit

---
+++ early-init.el
@@ -0,0 +1,8 @@
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/"))
+      gc-cons-threshold (* 50 1024 1024))
+
+
