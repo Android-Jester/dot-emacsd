f0dfa08a9f2340cf6a5d23f571141ccf7f580728
Duah Kwadwo Adjei <duah14@outlook.com>
2024-03-24 19:43:14 +0000

    Added some stuffs

---
+++ custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(corfu-auto t)
+ '(corfu-cycle t)
+ '(dired-listing-switches "-agho --group-directories-first")
+ '(display-line-numbers-type 'relative)
+ '(evil-undo-system 'undo-fu)
+ '(package-selected-packages
+   '(multi-vterm vterm-toggle org-modern zig-mode yasnippet-capf which-key web-mode wakatime-mode vterm vertico-prescient undo-fu treesit-auto tree-sitter toc-org solaire-mode smex rustic ranger rainbow-delimiters no-littering nerd-icons-corfu marginalia magit lua-mode ligature indent-guide highlight-indentation helpful general flycheck fish-mode evil-visual-mark-mode evil-commentary evil-collection evil-anzu eglot doom-themes doom-modeline dired-single dired-ranger denote dashboard dash-functional dart-mode corfu-prescient consult cape apheleia))
+ '(ranger-cleanup-eagerly t)
+ '(ranger-cleanup-on-disable t)
+ '(ranger-override-dired t)
+ '(ranger-show-hidden t)
+ '(treesit-auto-install 'prompt))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
